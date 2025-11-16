d7081c13ed9b432f6e714916e125cacd614ccaa7
irism <irismdev1@gmail.com>
2025-11-16 09:51:07 -0500

    Change build script

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ export default {
     darkMode: ["class"],
     content: [
       "./index.html",
-      "./**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./App.tsx",
+      "./main.tsx",
     ],
     theme: {
       extend: {
@@ -69,4 +72,3 @@ export default {
     },
     plugins: [require("tailwindcss-animate")],
   }
-  
